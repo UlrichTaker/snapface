3e70d671743b7078f010e66054797870de751c60
Abel François <abelitocoly@gmail.com>
2023-11-14 13:53:10 +0000

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [//Ce tableau va lier les routes de votre application (les différentes URL) aux components correspondants
-    { path: 'facesnaps', component: FaceSnapListComponent }//une première route  facesnaps  qui affichera le component FaceSnapListComponent 
+  { path: 'facesnaps', component: FaceSnapListComponent },//une première route  facesnaps  qui affichera le component FaceSnapListComponent 
+  { path: '',component: LandingPageComponent}//le component à afficher pour la route vide
 ];
 
 @NgModule({
