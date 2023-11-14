7101d934053d227d1eee0000047d9ca6a030c4a8
Abel François <abelitocoly@gmail.com>
2023-11-14 13:56:33 +0000

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
-  
+export class AppComponent implements OnInit {
+  interval$!: Observable<number>;//quand on déclare le type de  interval$  , on le déclare comme  Observable  qui émet des  number  en passant  number  entre chevrons <>  
+  ngOnInit() {
+    this.interval$ = interval(1000);//créer un Observable qui émet des nombres croissants, en passant le nombre de millisecondes qui doit séparer les émissions. Vous stockez cet Observable dans une constante nommée  interval$  
+  }
 }
+
